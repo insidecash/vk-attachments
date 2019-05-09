a45a473b8390adbc0e9029fe488c3dca54a78ef0
Konstantin Moskalenko <kkmoskalenko@icloud.com>
2019-05-09 11:05:30 +0700

    Move an attachments view to the separate component

---
+++ src/Components/Attachments/index.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import List from '../List';
+import { errors, VK } from 'vk-call';
+
+class Attachments extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { images: [] };
+  }
+
+  fetchImages(next) {
+    const { peerId } = this.props;
+
+    const api = new VK({
+      token: localStorage.getItem('user_token'),
+      version: '5.92'
+    });
+
+    const processItems = items =>
+      items.map(({ attachment }) => {
+        const { id, sizes } = attachment.photo;
+
+        sizes.sort((a, b) => {
+          const types = 'wzyrqpoxms';
+          return types.indexOf(a.type) - types.indexOf(b.type);
+        });
+
+        return {
+          id: id,
+          links: sizes.map(size => size.url)
+        };
+      });
+
+    api.call('messages.getHistoryAttachments', {
+      peer_id: peerId,
+      media_type: 'photo',
+      ...next && { start_from: next },
+      preserve_order: 1
+    })
+      .then(({ items, next_from }) => {
+        this.setState(({ images }) => ({
+          images: [
+            ...images,
+            ...processItems(items)
+          ]
+        }));
+        next_from && this.fetchImages(next_from);
+      })
+      .catch(error => {
+        const repeat = () => next && this.fetchImages(next);
+
+        console.error(error);
+        error.type === errors.TOO_MANY_REQUESTS ?
+          setTimeout(repeat, 1000) :
+          alert(error.message);
+      });
+  };
+
+  componentDidMount() {
+    this.fetchImages();
+  }
+
+  render() {
+    const { images } = this.state;
+    return <List images={images}/>;
+  }
+}
+
+export default Attachments;
