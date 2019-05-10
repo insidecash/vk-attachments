7bd9092a7da61e55309ec6a15a03ba8f92a90876
Konstantin Moskalenko <kkmoskalenko@icloud.com>
2019-05-10 16:37:47 +0700

    Add a User Token form

--- src/Components/App/index.jsx
+++ src/Components/App/index.jsx
@@ -6,11 +6,29 @@ import Conversations from '../Conversations';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { peerId: null };
+
+    this.state = {
+      token: localStorage.getItem('user_token') || '',
+      peerId: null
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({ token: target.value });
+  }
+
+  handleSubmit(event) {
+    localStorage.setItem('user_token', this.state.token);
+    window.location.reload(true);
+
+    event.preventDefault();
   }
 
   render() {
-    const { peerId } = this.state;
+    const { peerId, token } = this.state;
     const handleSelect = selectedOption =>
       this.setState({
         peerId: selectedOption.value
@@ -19,6 +37,15 @@ class App extends React.Component {
     return (
       <React.Fragment>
         <header>
+          <form onSubmit={this.handleSubmit} className='token-form'>
+            <input
+              type='text'
+              placeholder='User Token'
+              value={token}
+              onChange={this.handleChange}
+            />
+          </form>
+
           <Conversations peerId={peerId} onSelect={handleSelect}/>
         </header>
 
